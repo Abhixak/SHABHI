d953a55e559d5bfbd86b733b777e9d7c5a29936b
Abhixak <abhishekkumar2526105@gmail.com>
2025-05-16 18:06:27 +0530

    Added Icons and state

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import MainContainer from "./Components/MainContainer";
 
 
 function App() {
-
+  window.alert("This Application is Currently under Construction. You cannot Chat with anyone Yet, But you can just try using it.")
   return (
     <>
       <MainContainer />
--- src/Components/DisplayScreen.jsx
+++ src/Components/DisplayScreen.jsx
@@ -3,8 +3,17 @@ import "../Styles/FX.css";
 const DisplayScreen = () => {
 
   return (
-    <div className='screen overflow-hidden bg-[#fff8ec] h-[85%] absolute top-4 w-[90%] sm:h-[85%] sm:w-[95%] sm:absolute sm:top-4 lg:h-[95%] lg:static lg:w-[70%] rounded-2xl border-2 border-[#366871]'>
-      
+    <div className='
+     screen overflow-hidden
+     bg-[#fff8ec] h-[85%] 
+     absolute top-4 w-[90%] 
+     sm:h-[85%] sm:w-[95%] 
+     sm:absolute sm:top-4 lg:h-[95%] 
+     lg:static lg:w-[70%] rounded-2xl 
+     border-2 border-[#366871]
+     flex justify-center items-center
+    '>
+        DISPLAY SCREEN      
     </div>
   )
 }
