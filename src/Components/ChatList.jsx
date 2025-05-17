215a14a0a9d9061cfbeeafb317d5e8713802488f
Abhixak <abhishekkumar2526105@gmail.com>
2025-05-17 22:57:09 +0530

    Added Chat list

--- src/Components/ChatList.jsx
+++ src/Components/ChatList.jsx
@@ -3,14 +3,18 @@ import React from 'react'
 const ChatList = () => {
   return (
     <>
-        <ul className='!mt-10 w-full'>
-            <li className='border-2 !p-4 !m-2 rounded-xl'>Mohit</li>
-            <li className='border-2 !p-4 !m-2 rounded-xl'>Kishan</li>
-            <li className='border-2 !p-4 !m-2 rounded-xl'>tanish</li>
-            <li className='border-2 !p-4 !m-2 rounded-xl'>tanish</li>
-            <li className='border-2 !p-4 !m-2 rounded-xl'>Mohit</li>
-            <li className='border-2 !p-4 !m-2 rounded-xl'>Kishan</li>
-            <li className='border-2 !p-4 !m-2 rounded-xl'>tanish</li>
+    <h1 className='absolute !mt-3'>SHABHI</h1>
+        <ul className='!mt-10 w-full overflow-y-scroll text-[black]'>
+            <li className='!p-4 !m-2 rounded-xl bg-[#dfe0df]'>Friend 1</li>
+            <li className='!p-4 !m-2 rounded-xl bg-[#dfe0df]'>Friend 2</li>
+            <li className='!p-4 !m-2 rounded-xl bg-[#dfe0df]'>Friend 3</li>
+            <li className='!p-4 !m-2 rounded-xl bg-[#dfe0df]'>Friend 4</li>
+            <li className='!p-4 !m-2 rounded-xl bg-[#dfe0df]'>Friend 5</li>
+            <li className='!p-4 !m-2 rounded-xl bg-[#dfe0df]'>Friend 6</li>
+            <li className='!p-4 !m-2 rounded-xl bg-[#dfe0df]'>Friend 7</li>
+            <li className='!p-4 !m-2 rounded-xl bg-[#dfe0df]'>Friend 8</li>
+            <li className='!p-4 !m-2 rounded-xl bg-[#dfe0df]'>Friend 9</li>
+            <li className='!p-4 !m-2 rounded-xl bg-[#dfe0df]'>Friend 10</li>
         </ul> 
     </>
   )
